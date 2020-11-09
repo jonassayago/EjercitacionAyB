6b60575c13bea6847799d9335d0aba3ef8195fa2
JSabirached <josueabirached@gmail.com>
2020-11-09 12:43:28 -0300

    agregandoArchivo

---
+++ hora.js
@@ -0,0 +1,4 @@
+let horaActual = ()=> {
+    let fecha =new Date ();
+    return fecha .getHours()+ ":" + fecha.getMinutes();
+}
---
+++ suma.js
@@ -0,0 +1,3 @@
+let elDoble = num => num *2
+
+console.log(elDoble(5))
